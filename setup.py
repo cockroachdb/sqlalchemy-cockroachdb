d35f502ebef0bf83fe63bcf64e9b8a6c1678d4fe
Ben Darnell <ben@bendarnell.com>
2016-02-12 13:26:20 -0500

    Reset the repo (again)

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from setuptools import setup
-import sys
 
 install_requires = [
-    # protobuf 2.6 doesn't support python 3.
-    'protobuf==3.0.0-alpha-1',
-    'requests',
+    'psycopg2',
 ]
-
-if sys.version_info < (3, 4):
-    install_requires.append('enum34')
 
 setup(
     name='cockroach',
-    packages=['cockroach', 'cockroach.sql.driver'],
+    packages=['cockroach'],
     install_requires=install_requires,
-    extras_require={'tests': ['werkzeug']},
 )
