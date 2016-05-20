250a4f22764dbb38a0466bfbb8cb16a26a0145f1
Ben Darnell <ben@bendarnell.com>
2016-05-20 13:27:41 -0400

    Run the tests on circleci

---
+++ build/circle-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+pip install -r dev-requirements.txt
+
+COCKROACH_VERSION=beta-20160519
+COCKROACH_PLATFORM=linux-amd64
+COCKROACH_NAME=cockroach-${COCKROACH_VERSION}.${COCKROACH_PLATFORM}
+DOWNLOAD_DIR=~/cockroach-download
+
+if [ ! -x "${DOWNLOAD_DIR}/${COCKROACH_NAME}/cockroach" ]; then
+  mkdir -p "${DOWNLOAD_DIR}"
+  curl "https://binaries.cockroachdb.com/${COCKROACH_NAME}.tgz" | tar xzf - -C "${DOWNLOAD_DIR}"
+fi
+mkdir -p bin
+ln -sf "${DOWNLOAD_DIR}/${COCKROACH_NAME}/cockroach" bin
