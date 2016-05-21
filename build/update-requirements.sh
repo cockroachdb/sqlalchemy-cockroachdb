20d79f40ac01f1243a215a392c1e9dd1123fe088
Ben Darnell <ben@bendarnell.com>
2016-05-21 18:03:17 -0400

    Cleanup in a trap function.

--- build/update-requirements.sh
+++ build/update-requirements.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 ENV=$(mktemp -d)
 
+cleanup() {
+  rm -rf "${ENV}"
+}
+trap cleanup EXIT
+
 if [ ! -f "${1}.in" ]; then
   echo "input file $1.in not found"
   exit 1
@@ -12,4 +17,3 @@ fi
 virtualenv -p python3.5 "${ENV}"
 "${ENV}/bin/pip" install -r "${1}.in"
 "${ENV}/bin/pip" freeze -r "${1}.in" > "$1"
-rm -rf "${ENV}"
