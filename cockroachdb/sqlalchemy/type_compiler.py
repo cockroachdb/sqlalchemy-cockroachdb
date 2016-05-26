1f220597cc3aa0b3ad25bbb8ac72a2a1f21dd926
Ben Darnell <ben@bendarnell.com>
2016-05-25 22:14:55 -0400

    Enable decimal support

--- cockroachdb/sqlalchemy/type_compiler.py
+++ cockroachdb/sqlalchemy/type_compiler.py
@@ -9,16 +9,3 @@ class TypeCompiler(PGTypeCompiler):
 
     def visit_TIME(self, typ, **kw):
         return "TIME"
-
-    def visit_DECIMAL(self, type_, **kw):
-        # Map DECIMAL to FLOAT (which is something sqlalchemy supports
-        # for databases that don't have a native decimal type.
-        # We do, but the tests all fail if we try to use it. It looks
-        # like the tests rely on either implicit casts or type inference
-        # for literals.
-        #
-        # TODO(bdarnell): replace with DECIMAL when it works, perhaps
-        # once the new type system is in.
-        return "FLOAT"
-
-    visit_NUMERIC = visit_DECIMAL
