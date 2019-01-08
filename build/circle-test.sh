60a8ceb928dc61f5a7fdfa47e95430aaaf0c7a29
Christian Köhn <christian@ckoehn.de>
2019-01-08 22:44:46 +0100

    Remove hardcoded CockroachDB version in test stage

--- build/circle-test.sh
+++ build/circle-test.sh
@@ -4,14 +4,10 @@ set -eux -o pipefail
 
 source env/bin/activate
 
-# TODO(bdarnell): versions 2.0 and 1.1 currently fail because of the
-# LIKE ESCAPE operator, and we don't have a clean way to disable that
-# part of the test suite.
-
-for version in v2.1; do
+for executable in env/bin/cockroach-*; do
   rm -rf cockroach-data
-  "cockroach-$version" start --background --insecure --pid-file=cockroach.pid
-  "cockroach-$version" sql --insecure -e 'CREATE DATABASE IF NOT EXISTS test_sqlalchemy'
+  ${executable} start --background --insecure --pid-file=cockroach.pid
+  ${executable} sql --insecure -e 'CREATE DATABASE IF NOT EXISTS test_sqlalchemy'
 
   make test
   kill -9 $(cat cockroach.pid)
