590f5ee8f488da87e246bf85efae0e3c83f4766a
Ben Darnell <ben@bendarnell.com>
2019-02-25 14:56:51 -0500

    Set version number to 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name='cockroachdb',
-    version='0.3.0',
+    version='0.3.1',
     author='Cockroach Labs',
     author_email='cockroach-db@googlegroups.com',
     url='https://github.com/cockroachdb/cockroachdb-python',
