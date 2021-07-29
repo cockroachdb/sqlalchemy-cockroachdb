6afd98230665d9a76b13f2a453d764c4455b3e64
Rafi Shamim <rafi@cockroachlabs.com>
2021-07-29 19:47:53 -0400

    Begin v1.4.1 development

--- sqlalchemy_cockroachdb/__init__.py
+++ sqlalchemy_cockroachdb/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy.dialects import registry as _registry
 from .transaction import run_transaction  # noqa
 
-__version__ = "1.4.0"
+__version__ = "1.4.1.dev0"
 
 _registry.register(
     "cockroachdb.psycopg2",
