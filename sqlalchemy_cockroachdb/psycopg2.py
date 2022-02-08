27f82b6f6be52cb3bdff4fe7de7ff12335467271
Jonathan Dieter <jonathan.dieter@spearline.com>
2022-02-08 22:04:45 +0000

    Make index hinting work with CockroachDB (#178)
    This includes support for asyncpg and testing.
    
    Signed-off-by: Jonathan Dieter <jonathan.dieter@spearline.com>
    Co-authored-by: Gord Thompson <gord@gordthompson.com>

--- sqlalchemy_cockroachdb/psycopg2.py
+++ sqlalchemy_cockroachdb/psycopg2.py
@@ -1,6 +1,7 @@
 from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
 from .base import CockroachDBDialect
 from .ddl_compiler import CockroachDDLCompiler
+from .stmt_compiler import CockroachCompiler
 from .stmt_compiler import CockroachIdentifierPreparer
 
 
@@ -8,5 +9,6 @@ class CockroachDBDialect_psycopg2(PGDialect_psycopg2, CockroachDBDialect):
     driver = "psycopg2"  # driver name
     preparer = CockroachIdentifierPreparer
     ddl_compiler = CockroachDDLCompiler
+    statement_compiler = CockroachCompiler
 
     supports_statement_cache = True
---
+++ test/test_with_hint.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column
+from sqlalchemy import Index
+from sqlalchemy import Integer
+from sqlalchemy import select
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy.testing import AssertsCompiledSQL
+from sqlalchemy.testing import fixtures
+from sqlalchemy.testing import provide_metadata
+
+
+class WithHintTest(fixtures.TestBase, AssertsCompiledSQL):
+    # __requires__ = ("sync_driver",)
+
+    @provide_metadata
+    def test_with_hint(self):
+        meta = self.metadata
+        t = Table(
+            "t",
+            meta,
+            Column("id", Integer),
+            Column("txt", String(50)),
+            Index("ix_t_txt", "txt"),
+        )
+        self.assert_compile(
+            select(t).with_hint(t, "ix_t_txt"),
+            "SELECT t.id, t.txt FROM t@ix_t_txt",
+        )
+        self.assert_compile(
+            select(t).with_hint(t, "ix_t_txt").where(t.c.id < 3),
+            "SELECT t.id, t.txt FROM t@ix_t_txt WHERE t.id < %(id_1)s",
+        )
