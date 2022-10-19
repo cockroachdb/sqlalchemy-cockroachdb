d61f2d23c08564b1058a81b08c749d2a12a0a073
Gord Thompson <gord@gordthompson.com>
2022-10-19 09:51:56 -0600

    Update setup.py for minimum Python version

--- setup.py
+++ setup.py
@@ -23,10 +23,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="SQLAlchemy CockroachDB",
     project_urls={
