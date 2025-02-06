6a9db5877ba7bdd604e94fc9d4fe2ae404600dfc
Gord Thompson <gord@gordthompson.com>
2025-02-06 14:10:50 -0700

    Apply updates for SQLA v2.0.38

--- sqlalchemy_cockroachdb/provision.py
+++ sqlalchemy_cockroachdb/provision.py
@@ -1,13 +1,6 @@
 from sqlalchemy.testing.provision import temp_table_keyword_args
-from sqlalchemy.testing.provision import update_db_opts
 
 
 @temp_table_keyword_args.for_db("cockroachdb")
 def _cockroachdb_temp_table_keyword_args(cfg, eng):
     return {"prefixes": ["TEMPORARY"]}
-
-
-@update_db_opts.for_db("cockroachdb")
-def _update_db_opts(db_url, db_opts, options):
-    """Set database options (db_opts) for a test database that we created."""
-    db_opts["isolation_level"] = "SERIALIZABLE"
