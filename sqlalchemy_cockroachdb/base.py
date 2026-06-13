6ed53203ce67bdc925b4afaa90b8f4c3d3a6cfce
Gord Thompson <gord@gordthompson.com>
2026-06-13 09:28:42 -0600

    Preliminary updates for CRDB v26.3

--- sqlalchemy_cockroachdb/base.py
+++ sqlalchemy_cockroachdb/base.py
@@ -98,6 +98,7 @@ class CockroachDBDialect(PGDialect):
         self._is_v254plus = self._is_v253plus and (" v25.3." not in sversion)
         self._is_v261plus = self._is_v254plus and (" v25.4." not in sversion)
         self._is_v262plus = self._is_v261plus and (" v26.1." not in sversion)
+        self._is_v263plus = self._is_v262plus and (" v26.2." not in sversion)
         self._has_native_json = self._is_v2plus
         self._has_native_jsonb = self._is_v2plus
         self._supports_savepoints = self._is_v201plus
--- test/test_column_reflect.py
+++ test/test_column_reflect.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
     Identity,
     Computed,
 )
-from sqlalchemy.testing import fixtures, eq_
+from sqlalchemy.testing import fixtures, eq_, config
 
 meta = MetaData()
 
@@ -60,7 +60,7 @@ with_computed_stored = Table(
 #       set the 'persisted' flag to None or True for CockroachDB support.
 
 
-class ReflectHiddenColumnsTest(fixtures.TestBase):
+class ReflectSpecialColumnsTest(fixtures.TestBase):
     __requires__ = ("sync_driver",)
 
     def teardown_method(self, method):
@@ -69,9 +69,9 @@ class ReflectHiddenColumnsTest(fixtures.TestBase):
     def setup_method(self):
         meta.create_all(testing.db)
 
-    def _get_col_info(self, table_name, include_hidden=False):
+    def _get_col_info(self, table_name, schema=None, include_hidden=False):
         insp = inspect(testing.db)
-        col_info = insp.get_columns(table_name, include_hidden=include_hidden)
+        col_info = insp.get_columns(table_name, schema, include_hidden=include_hidden)
         for row in col_info:
             row["type"] = str(row["type"])
         return col_info
@@ -140,23 +140,24 @@ class ReflectHiddenColumnsTest(fixtures.TestBase):
             ],
         )
 
-    def test_reflect_identity(self):
+    def test_two_hidden(self):
+        with testing.db.begin() as conn:
+            conn.exec_driver_sql(
+                "create table tmp ("
+                "id int primary key,"
+                "hidden1 int not visible,"
+                "hidden2 int not visible,"
+                "int_col int"
+                ")"
+            )
+
         eq_(
-            self._get_col_info("with_identity"),
+            self._get_col_info("tmp", include_hidden=False),
             [
                 {
-                    "autoincrement": True,
+                    "autoincrement": False,
                     "comment": None,
-                    "default": "nextval('public.with_identity_id_seq'::REGCLASS)",
-                    "identity": {
-                        "always": False,
-                        "cache": 1,
-                        "cycle": False,
-                        "increment": 1,
-                        "maxvalue": 9223372036854775807,
-                        "minvalue": 1,
-                        "start": 1,
-                    },
+                    "default": None,
                     "is_hidden": False,
                     "name": "id",
                     "nullable": False,
@@ -167,45 +168,83 @@ class ReflectHiddenColumnsTest(fixtures.TestBase):
                     "comment": None,
                     "default": None,
                     "is_hidden": False,
-                    "name": "txt",
+                    "name": "int_col",
                     "nullable": True,
-                    "type": "VARCHAR",
+                    "type": "INTEGER",
                 },
             ],
         )
 
-        eq_(
-            self._get_col_info("with_identity_always"),
-            [
-                {
-                    "autoincrement": True,
-                    "comment": None,
-                    "default": "nextval('public.with_identity_always_id_seq'::REGCLASS)",
-                    "identity": {
-                        "always": True,
-                        "cache": 1,
-                        "cycle": False,
-                        "increment": 1,
-                        "maxvalue": 9223372036854775807,
-                        "minvalue": 1,
-                        "start": 1,
+        with testing.db.begin() as conn:
+            conn.exec_driver_sql("drop table tmp")
+
+    def test_reflect_identity(self):
+        if config.db.dialect._is_v263plus:
+            eq_(
+                self._get_col_info("with_identity"),
+                [
+                    {
+                        "autoincrement": True,
+                        "comment": None,
+                        "default": None,
+                        "identity": {
+                            "always": False,
+                            "cache": 1,
+                            "cycle": False,
+                            "increment": 1,
+                            "maxvalue": 9223372036854775807,
+                            "minvalue": 1,
+                            "start": 1,
+                        },
+                        "is_hidden": False,
+                        "name": "id",
+                        "nullable": False,
+                        "type": "INTEGER",
                     },
-                    "is_hidden": False,
-                    "name": "id",
-                    "nullable": False,
-                    "type": "INTEGER",
-                },
-                {
-                    "autoincrement": False,
-                    "comment": None,
-                    "default": None,
-                    "is_hidden": False,
-                    "name": "txt",
-                    "nullable": True,
-                    "type": "VARCHAR",
-                },
-            ],
-        )
+                    {
+                        "autoincrement": False,
+                        "comment": None,
+                        "default": None,
+                        "is_hidden": False,
+                        "name": "txt",
+                        "nullable": True,
+                        "type": "VARCHAR",
+                    },
+                ],
+            )
+
+            eq_(
+                self._get_col_info("with_identity_always"),
+                [
+                    {
+                        "autoincrement": True,
+                        "comment": None,
+                        "default": None,
+                        "identity": {
+                            "always": True,
+                            "cache": 1,
+                            "cycle": False,
+                            "increment": 1,
+                            "maxvalue": 9223372036854775807,
+                            "minvalue": 1,
+                            "start": 1,
+                        },
+                        "is_hidden": False,
+                        "name": "id",
+                        "nullable": False,
+                        "type": "INTEGER",
+                    },
+                    {
+                        "autoincrement": False,
+                        "comment": None,
+                        "default": None,
+                        "is_hidden": False,
+                        "name": "txt",
+                        "nullable": True,
+                        "type": "VARCHAR",
+                    },
+                ],
+            )
 
     def test_reflect_computed_stored(self):
         eq_(
@@ -233,29 +272,29 @@ class ReflectHiddenColumnsTest(fixtures.TestBase):
             ],
         )
 
-    # def test_reflect_other_schema(self):
-    #     # TODO: uncomment when resolved
-    #     # verify https://github.com/cockroachdb/cockroach/issues/170049
-    #     eq_(
-    #         self._get_col_info("with_pk_other_schema"),
-    #         [
-    #             {
-    #                 "autoincrement": True,
-    #                 "comment": None,
-    #                 "default": "unique_rowid()",
-    #                 "is_hidden": False,
-    #                 "name": "id",
-    #                 "nullable": False,
-    #                 "type": "INTEGER",
-    #             },
-    #             {
-    #                 "autoincrement": False,
-    #                 "comment": None,
-    #                 "default": None,
-    #                 "is_hidden": False,
-    #                 "name": "txt",
-    #                 "nullable": True,
-    #                 "type": "VARCHAR",
-    #             },
-    #         ],
-    #     )
+    def test_reflect_other_schema(self):
+        # verify https://github.com/cockroachdb/cockroach/issues/170049
+        if config.db.dialect._is_v263plus:
+            eq_(
+                self._get_col_info("with_pk_other_schema", schema="test_schema"),
+                [
+                    {
+                        "autoincrement": True,
+                        "comment": None,
+                        "default": "unique_rowid()",
+                        "is_hidden": False,
+                        "name": "id",
+                        "nullable": False,
+                        "type": "INTEGER",
+                    },
+                    {
+                        "autoincrement": False,
+                        "comment": None,
+                        "default": None,
+                        "is_hidden": False,
+                        "name": "txt",
+                        "nullable": True,
+                        "type": "VARCHAR",
+                    },
+                ],
+            )
